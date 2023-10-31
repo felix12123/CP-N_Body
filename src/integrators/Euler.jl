142459d9c0267820c61b86dd451b8a2ee9d3f608
Jan Z <jan.znz@hotmail.com>
2023-10-31 15:28:50 +0100

    Update Velocity-Verlet
    Velocity-Verlet wurde für variable Zeitschritte modifiziert. Für konstante Zeitschritte wurde die Rechenvorschrift jedoch nicht gelöscht, sondern lediglich auskommentiert.

--- src/integrators/Euler.jl
+++ src/integrators/Euler.jl
@@ -76,14 +76,33 @@ function integ_velocity_verlet(body0, force, dt)
 	m =  body[3]
 	a0 = (force(body) ./ m)
 
-	# Berechnungsvorschrift für den neuen Ort:
-	x1 = x0 .+ (v0 .* dt) + ((1/2) * a0 * dt^2)
+	# **********************************************************************
+	# Velocity-Verlet für konstante Zeitschritte:
+	#	# Berechnungsvorschrift für den neuen Ort:
+	#	x1 = x0 .+ (v0 .* dt) + ((1/2) * a0 * dt^2)
 
-	# Schreibe neues x (= x1) in body, um neues a (= a1) zu berechnen.
-	# Dieses benötigt man um neues v (= v1) zu berechnen:
+	#	# Schreibe neues x (= x1) in body, um neues a (= a1) zu berechnen.
+	#	# Dieses benötigt man um neues v (= v1) zu berechnen:
+	#	body[1] = x1
+	#	a1  = (force(body) ./ m)
+	#	v1 = v0 .+ ((1/2) * mean.(a0, a1) * dt)
+
+	#	# Schreibe neues v (= v1) in body:
+	#	body[2] = v1
+
+	#	# Gebe Ergebnis zurück:
+	#	return body
+	# **********************************************************************
+
+	# Velocity-Verlet für variable Zeitschritte (kick-drift-kick-Methode):
+	v1_05 = v0 .+ ((1/2) .* a0 .* dt)			# (1.38)
+	x1 	  = x0 .+ (v1_05 .* dt)					# (1.39)
+
+	#	# Schreibe neues x (= x1) in body, um neues a (= a1) zu berechnen.
+	#	# Dieses benötigt man um neues v (= v1) zu berechnen:
 	body[1] = x1
 	a1  = (force(body) ./ m)
-	v1 = v0 .+ ((1/2) * mean.(a0, a1) * dt)
+	v1 = v1_05 .+ (1/2 .* a1 .* dt)				# (1.40)
 
 	# Schreibe neues v (= v1) in body:
 	body[2] = v1
@@ -161,21 +180,21 @@ function integ_rk4(body0, force, dt)
 	x1 = x0 .+ (v0 .* dt_2)								# Berechne x1 via: x1 = x0 + v0*(dt/2)
 	v1 = v0 .+ (a0 .* dt_2)								# Berechne v1 via: v1 = v0 + a0*(dt/2)
 	body[1] = x1										# Schreibe x1 in body, damit a1 berechnet werden kann
-	#body[2] = v1										# Schreibe v1 in body für spätere Berechnungen von x2
+	#body[2] = v1										# Schreibe v1 in body, brauchen wir jedoch nicht
 	a1  = (force(body) ./ m)							# Berechne a1 über die Kraft: f/m = a
 
 	# Ermittel nun K3:
 	x2 = x0 .+ (v1 .* dt_2)								# x2 = x0 + v1*(dt/2)
 	v2 = v0 .+ (a1 .* dt_2)								# Berechne v2 via: v2 = v0 + a1*(dt/2)
 	body[1] = x2										# Schreibe x2 in body, damit a2 berechnet werden kann
-	#body[2] = v2										# Schreibe v2 in body für spätere Berechnungen von x3
+	#body[2] = v2										# Schreibe v2 in body, brauchen wir jedoch nicht
 	a2 = (force(body) ./ m)								# Berechne a2 über die Kraft: f/m = a
 
 	# Ermittel nun K4:
 	x3 = x0 .+ (v2 .* dt)								# Berechne x3 via: x3 = x0 + v2*dt
 	v3 = v0 .+ (a2 .* dt)								# Berechne v3 via: v3 = v0 + a2*dt
 	body[1] = x3										# Schreibe x3 in body, damit a3 berechnet werden kann
-	#body[2] = v3										# Schreibe v3 in body
+	#body[2] = v3										# Schreibe v3 in body, brauchen wir jedoch nicht
 	a3 = (force(body) ./ m)								# Berechne a3 über die Kraft: f/m = a
 
 	# Berechne nun Vorhersage nach RK4:
