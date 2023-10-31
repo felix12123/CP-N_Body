3ce3e235d1c6deb7ddf23e4cf691538f07bb3a7f
Jan Z <jan.znz@hotmail.com>
2023-10-31 14:36:05 +0100

    Auslagerung integ_euler! + Neue integratoren
    Code mit euler-integrator, welcher die Eingabe verändert ausgelagert, damit dieser nicht verloren geht aber auch nicht das Bild des Codes stört.
    
    Darüber hinaus wurden Euler-Cromer und Velocity-Verlet implementiert.

--- src/integrators/Euler.jl
+++ src/integrators/Euler.jl
@@ -4,18 +4,21 @@
 
 
 
-# ****************************
-# ***** Euler-Integrator *****
-# ****************************
+# *****************
+# ***** Euler *****
+# *****************
 
 function integ_euler(body0, force, dt)
 
 	# Fertige Deepcopy von Body an, um aliasing zu vermeiden:
 	body = deepcopy(body0)
 
+	# Damit body[2] nicht 2x ausgelesen werden muss:
+	v0 = body[2]
+
 	# Berechne die neuen Orte und Geschwindigkeiten:
-	x1 = body[1] .+ (body[2] .* dt)								# x1 = x + v*dt
-	v1 = body[1] .+ ((force(body) ./ body[3]) .* dt)			# v1 = v + a*dt = v + (f/m)*dt
+	v1 = v0 .+ ((force(body) ./ body[3]) .* dt)				# v1 = v + a*dt = v + (f/m)*dt
+	x1 = body[1] .+ (v0 .* dt)								# x1 = x + v*dt
 
 	# Schreibe neue Werte in body:
 	body[1] = x1
@@ -25,17 +28,70 @@ function integ_euler(body0, force, dt)
 	return body
 end
 
-# Euler-Integrator, bei dem die Eingabe überschrieben wird (benutzen wir wahrschienlich nicht):
-function integ_euler!(body, force, dt)
+
+
+
+
+# ************************
+# ***** Euler-Cromer *****
+# ************************
+
+function integ_euler_cromer(body0, force, dt)
+
+	# Fertige Deepcopy von Body an, um aliasing zu vermeiden:
+	body = deepcopy(body0)
+
+	# Damit body[2] nicht 2x ausgelesen werden muss:
+	v0 = body[2]
 
 	# Berechne die neuen Orte und Geschwindigkeiten:
-	x1 = body[1] .+ (body[2] .* dt)							# x1 = x + v*dt
-	v1 = body[1] .+ ((force(body) ./ body[3]) .* dt)		# v1 = v + a*dt = v + (f/m)*dt
+	v1 = v0 .+ ((force(body) ./ body[3]) .* dt)				# v1 = v0 + a0*dt = v + (f/m)*dt
+	x1 = body[1] .+ (mean.(v0, v1) .* dt)					# x1 = x0 + (1/2)*(v0 + v1)dt
 
 	# Schreibe neue Werte in body:
 	body[1] = x1
 	body[2] = v1
+
+	# Gebe body zurück:
+	return body
 end
+
+
+
+
+
+# ***************************
+# ***** Velocity-Verlet *****
+# ***************************
+
+function integ_velocity_verlet(body0, force, dt)
+	
+	# Fertige Deepcopy von Body an, um aliasing zu vermeiden:
+	body = deepcopy(body0)
+
+	# Erforderliche Größen:
+	# Speichere x0, v0 und a0:
+	x0 = body[1]
+	v0 = body[2]
+	m =  body[3]
+	a0 = (force(body) ./ m)
+
+	# Berechnungsvorschrift für den neuen Ort:
+	x1 = x0 .+ (v0 .* dt) + ((1/2) * a0 * dt^2)
+
+	# Schreibe neues x (= x1) in body, um neues a (= a1) zu berechnen.
+	# Dieses benötigt man um neues v (= v1) zu berechnen:
+	body[1] = x1
+	a1  = (force(body) ./ m)
+	v1 = v0 .+ ((1/2) * mean.(a0, a1) * dt)
+
+	# Schreibe neues v (= v1) in body:
+	body[2] = v1
+
+	# Gebe Ergebnis zurück:
+	return body
+end
+
 
 
 
@@ -44,6 +100,8 @@ end
 # *************************
 # ***** Runge-Kutta 2 *****
 # *************************
+
+# Hier der RK2-Integrator bzw. Heun-Integrator
 
 function integ_rk2(body0, force, dt)
 
@@ -69,9 +127,12 @@ function integ_rk2(body0, force, dt)
 	xnew = x0 + (dt/2) * mean.(v0, v1)
 	vnew = v0 + (dt/2) * mean.(a0, a1)
 
-	# Schreibe gemittelte Werte in body rein für das finale Ergebnis:
+	# Schreibe neue Werte in body:
 	body[1] = xnew
 	body[2] = vnew
+
+	# Gebe Ergebnis zurück:
+	return body
 end
 
 
@@ -100,21 +161,21 @@ function integ_rk4(body0, force, dt)
 	x1 = x0 .+ (v0 .* dt_2)								# Berechne x1 via: x1 = x0 + v0*(dt/2)
 	v1 = v0 .+ (a0 .* dt_2)								# Berechne v1 via: v1 = v0 + a0*(dt/2)
 	body[1] = x1										# Schreibe x1 in body, damit a1 berechnet werden kann
-	body[2] = v1										# Schreibe v1 in body für spätere Berechnungen von x2
+	#body[2] = v1										# Schreibe v1 in body für spätere Berechnungen von x2
 	a1  = (force(body) ./ m)							# Berechne a1 über die Kraft: f/m = a
 
 	# Ermittel nun K3:
 	x2 = x0 .+ (v1 .* dt_2)								# x2 = x0 + v1*(dt/2)
 	v2 = v0 .+ (a1 .* dt_2)								# Berechne v2 via: v2 = v0 + a1*(dt/2)
 	body[1] = x2										# Schreibe x2 in body, damit a2 berechnet werden kann
-	body[2] = v2										# Schreibe v2 in body für spätere Berechnungen von x3
+	#body[2] = v2										# Schreibe v2 in body für spätere Berechnungen von x3
 	a2 = (force(body) ./ m)								# Berechne a2 über die Kraft: f/m = a
 
 	# Ermittel nun K4:
 	x3 = x0 .+ (v2 .* dt)								# Berechne x3 via: x3 = x0 + v2*dt
 	v3 = v0 .+ (a2 .* dt)								# Berechne v3 via: v3 = v0 + a2*dt
 	body[1] = x3										# Schreibe x3 in body, damit a3 berechnet werden kann
-	body[2] = v3										# Schreibe v3 in body
+	#body[2] = v3										# Schreibe v3 in body
 	a3 = (force(body) ./ m)								# Berechne a3 über die Kraft: f/m = a
 
 	# Berechne nun Vorhersage nach RK4:
